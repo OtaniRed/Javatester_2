b53c4a8db4eab74d9a0f6385e0ac8425029fde08
OtaniRed <39827443+OtaniRed@users.noreply.github.com>
2018-08-08 20:34:40 +0300

    редактирование

--- 2work/sandbox/src/main/java/ru/stg/pft/sandbox/myfirstprogram.java
+++ 2work/sandbox/src/main/java/ru/stg/pft/sandbox/myfirstprogram.java
@@ -1,9 +1,9 @@
 package ru.stg.pft.sandbox;
 
-public class myfirstprogram
+public class Myfirstprogram
 {
-   public static void main(String[] args)
-{
-     System.out.println("hello world");
-}
+  public static void main(String[] args)
+  {
+    System.out.println("hello world");
+  }
 }
